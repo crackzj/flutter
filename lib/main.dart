b547987a498fe159ad509a01f5af179a017d4746
crackzj <crackzj@qq.com>
2021-01-07 21:03:00 +0800

    02_案例

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,74 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-  runApp(MaterialApp(
-    title: "hello world",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("我是标题"),
+void main(List<String> args) => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("卜夋"),
+        ),
+        body: HomeContent(),
       ),
-      body: Center(
-          child: Text(
-        "hello world",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 20, color: Colors.deepOrange),
-      )),
-    ),
-  ));
+    );
+  }
+}
+
+class HomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+        padding: EdgeInsets.all(10),
+        child: ListView(
+          children: [
+            ProductItem("Apple1", "macbook air 13`",
+                "https://cdn.jsdelivr.net/gh/crackzj/blogImg/blog20201223211137.jpeg"),
+            ProductItem("Apple2", "macbook pro 13`",
+                "https://cdn.jsdelivr.net/gh/crackzj/blogImg/blog20201223211137.jpeg"),
+            ProductItem("Apple3", "macbook pro 16`",
+                "https://cdn.jsdelivr.net/gh/crackzj/blogImg/blog20201223211137.jpeg")
+          ],
+        ));
+  }
+}
+
+class ProductItem extends StatelessWidget {
+  final String title;
+  final String content;
+  final String url;
+  ProductItem(this.title, this.content, this.url);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      decoration: BoxDecoration(border: Border.all()),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 24,
+            ),
+          ),
+          Text(
+            content,
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Image.network(url)
+        ],
+      ),
+    );
+  }
 }
