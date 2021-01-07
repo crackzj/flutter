e53f3170b906ff9ba2cb020ad70d0d7bc666b27a
crackzj <crackzj@qq.com>
2021-01-07 20:16:24 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    title: "hello world",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("我是标题"),
+      ),
+      body: Center(
+          child: Text(
+        "hello world",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 20, color: Colors.deepOrange),
+      )),
+    ),
+  ));
+}
+
+Widget 
